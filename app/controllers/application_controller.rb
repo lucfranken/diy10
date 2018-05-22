333be1770274b0fccd345553cd3fc92b395f844e
lucfranken <lucfranken@tentoday.com>
2018-05-22 16:31:01 +0200

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,15 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :set_current_user
 
+  before_action :set_locale
+
   private
 
   def set_current_user
     Current.user = current_user
+  end
+   
+  def set_locale
+    I18n.locale = :nl || I18n.default_locale
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,6 @@ module Myapp
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    config.i18n.default_locale = :nl
+    config.i18n.default_locale = :en
   end
 end
